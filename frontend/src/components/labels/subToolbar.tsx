e4b109f2fe298f81324e5a44e49e25a6d90067e7
Firooze Javaheri <firooze.javaheri@gmail.com>
2022-07-02 16:57:56 +0430

    Remove test module

--- frontend/src/components/labels/subToolbar.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { ThemeContext } from "./Labels";
-
-const SubToolbar = () => {
-    //const value = useContext(ThemeContext);
-    //console.log("rerender subtoolbar");
-    return <></>;
-};
-export default SubToolbar;
