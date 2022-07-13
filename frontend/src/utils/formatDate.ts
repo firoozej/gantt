0fd8dbabc8e13e295b06e53a1a89829e465f0253
Firooze Javaheri <firooze.javaheri@gmail.com>
2022-07-13 21:11:23 +0430

    Fix formatDate

--- frontend/src/utils/formatDate.ts
+++ frontend/src/utils/formatDate.ts
@@ -1,5 +1,5 @@
 import moment from "moment";
 
-export const formatDate = (isoDate: string, format: string = "YYYY-mm-DD") => {
+export const formatDate = (isoDate: string, format: string = "YYYY/MM/DD") => {
     return moment(isoDate).isValid() ? moment(isoDate).format(format) : null;
 };
